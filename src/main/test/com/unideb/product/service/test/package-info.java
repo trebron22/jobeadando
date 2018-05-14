29f13d72a12ff5cdbad6606af97c70a96134ce4d
gagyi.norbert <gagyi.norbert@DESKTOP-H76F8TV>
2018-05-14 08:51:15 +0200

    asd

---
+++ src/main/test/com/unideb/product/service/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @since 1.0
+ * @author gagyi.norbert
+ * @version 1.0
+ * This package test productService functions.
+ */
+package com.unideb.product.service.test;
---
+++ src/main/test/com/unideb/user/service/test/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * this package test userservice functions.
+ * @author gagyi.norbert
+ *
+ */
+package com.unideb.user.service.test;
